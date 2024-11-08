2fe0d56fdf4f0473a3a687b6892a21001bef06f7
YaCnDehfuli <mryd71114@gmail.com>
2024-11-08 18:42:37 -0500

    Add core requirements and Volatility preprocessing config

---
+++ Data Preprocessing/Dumps_to_Cnosolidated/config.py
@@ -0,0 +1,4 @@
+BASE_DIR = "/run/media/adam/My Book Duo/BCCC-Mal-NetMem-2025-Trojan-Onwards"
+OUTPUT_DIR = "/home/adam/BCCC_Dataset"
+CONSOLIDATED_DIR = "/home/adam/BCCC_Consolidated_Dataset"
+VOLATILITY = "/home/adam/volatility3/vol.py"
